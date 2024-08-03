c543ec85c962349091fd98b7dcd450fa02f011f6
Hiu Fung Chan <hfchan3@myseneca.ca>
2024-08-03 12:57:25 -0400

    Update Collection.h

--- WS03/part 2/Collection.h
+++ WS03/part 2/Collection.h
@@ -2,7 +2,7 @@
 Name:			    Hiu Fung CHAN
 Seneca Email :	    hfchan3@myseneca.ca
 Seneca Student ID : 106184237
-Date :				27 May 2024
+Date :              27 May 2024
 
 I declare that this submission is the result of my own work and I only copied the code that my professor provided to complete my
 workshops and assignments.This submitted piece of work has not been shared with any other student or 3rd party content provider.
